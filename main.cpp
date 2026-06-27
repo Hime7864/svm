1212344070795bc428275e8914c60e2a6b9c6df5
Steven Richter <159477578+Hime7864@users.noreply.github.com>
2026-06-27 12:34:31 -0700

    Commit

--- main.cpp
+++ main.cpp
@@ -28,7 +28,7 @@ bool NAKED is_zero_page(PVOID page)
     }
 }
 
-void FilterRawRanges(PHYSICAL_MEMORY_RANGE* range_0, PHYSICAL_MEMORY_RANGE* range_1)
+void FilterRawRanges(PHYSICAL_MEMORY_RANGE* range_0)
 {
     auto rva = MmMapIoSpace(range_0->BaseAddress.QuadPart, range_0->NumberOfBytes.QuadPart, MmNonCached);
     if (rva)
@@ -58,42 +58,11 @@ void FilterRawRanges(PHYSICAL_MEMORY_RANGE* range_0, PHYSICAL_MEMORY_RANGE* rang
 		range_0->BaseAddress.QuadPart = largest_zero_range_start;
 		range_0->NumberOfBytes.QuadPart = largest_zero_range * 4096;
     }
-
-
-    rva = MmMapIoSpace(range_1->BaseAddress.QuadPart, range_1->NumberOfBytes.QuadPart, MmNonCached);
-    if (rva)
-    {
-        int largest_zero_range = 0;
-        int current_zero_range = 0;
-        UINT64 largest_zero_range_start = 0;
-        for (UINT64 current = (UINT64)rva; current < (UINT64)rva + range_1->NumberOfBytes.QuadPart; current += 4096)
-        {
-            if (is_zero_page((PVOID)current))
-            {
-                current_zero_range++;
-            }
-            else
-            {
-                if (current_zero_range > largest_zero_range)
-                {
-                    largest_zero_range = current_zero_range;
-                    largest_zero_range_start = (current - (current_zero_range * 4096)) - (UINT64)rva + range_1->BaseAddress.QuadPart;
-                }
-                current_zero_range = 0;
-            }
-        }
-
-        MmUnmapIoSpace((PVOID)rva, range_1->NumberOfBytes.QuadPart);
-
-		range_1->BaseAddress.QuadPart = largest_zero_range_start;
-		range_1->NumberOfBytes.QuadPart = largest_zero_range * 4096;
-    }
     return;
 }
 
-bool ReclaimFirmwareMemory(PHYSICAL_MEMORY_RANGE* safe_data, PHYSICAL_MEMORY_RANGE* safe_executable)
+bool ReclaimFirmwareMemory(PHYSICAL_MEMORY_RANGE* range_0)
 {
-    safe_data->NumberOfBytes.QuadPart = 0;
     PHYSICAL_MEMORY_RANGE* pmr = MmGetPhysicalMemoryRanges();
     pmr += 2;
     int module_count = 0;
@@ -111,7 +80,6 @@ bool ReclaimFirmwareMemory(PHYSICAL_MEMORY_RANGE* safe_data, PHYSICAL_MEMORY_RAN
         if (size > 0x40000000)
             size = 0x40000000;
 
-		safe_data->BaseAddress.QuadPart = low;
         auto rva = (UINT64)MmMapIoSpace(low, size, MmNonCached);
         if (rva)
         {
@@ -126,18 +94,15 @@ bool ReclaimFirmwareMemory(PHYSICAL_MEMORY_RANGE* safe_data, PHYSICAL_MEMORY_RAN
                         sizeofimage = (sizeofimage & ~0xFFF) + 0x1000;
 
                     if (module_count == 0)
-                        safe_data->NumberOfBytes.QuadPart = (low + (current - rva)) - safe_data->BaseAddress.QuadPart;
-
-                    if (module_count == 0)
                     {
-                        safe_executable->BaseAddress.QuadPart = low + (current - rva) + sizeofimage;
+                        range_0->BaseAddress.QuadPart = low + (current - rva) + sizeofimage;
                     }
                     else if (module_count == 1)
                     {
-                        safe_executable->NumberOfBytes.QuadPart = (low + (current - rva)) - safe_executable->BaseAddress.QuadPart;
+                        range_0->NumberOfBytes.QuadPart = (low + (current - rva)) - range_0->BaseAddress.QuadPart;
                         MmUnmapIoSpace((PVOID)rva, size);
 
-						FilterRawRanges(safe_data, safe_executable);
+						FilterRawRanges(range_0);
                         return true;
                     }
                     module_count++;
@@ -154,11 +119,10 @@ bool ReclaimFirmwareMemory(PHYSICAL_MEMORY_RANGE* safe_data, PHYSICAL_MEMORY_RAN
 
 NTSTATUS DriverEntry()
 {
-    PHYSICAL_MEMORY_RANGE safe_range[2]{0};
-    if (ReclaimFirmwareMemory(&safe_range[0], &safe_range[1]))
+    PHYSICAL_MEMORY_RANGE range;
+    if (ReclaimFirmwareMemory(&range))
     {
-		printf("range 0: %p-%p size=%x\n", safe_range[0].BaseAddress.QuadPart, safe_range[0].BaseAddress.QuadPart - safe_range[0].NumberOfBytes.QuadPart, safe_range[0].NumberOfBytes.QuadPart);
-		printf("range 1: %p-%p size=%x\n", safe_range[1].BaseAddress.QuadPart, safe_range[1].BaseAddress.QuadPart - safe_range[1].NumberOfBytes.QuadPart, safe_range[1].NumberOfBytes.QuadPart);
+		printf("range: %p-%p size=%x pages=%i\n", range.BaseAddress.QuadPart, range.BaseAddress.QuadPart - range.NumberOfBytes.QuadPart, range.NumberOfBytes.QuadPart, range.NumberOfBytes.QuadPart / 4096);
     }
     return STATUS_SUCCESS;
 }
